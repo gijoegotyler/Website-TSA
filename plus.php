53f74fdbdc72c093e6d52fe507581af042b64ea6
gijoegotyler <33065655+gijoegotyler@users.noreply.github.com>
2017-10-26 14:08:55 -0600

    Add files via upload

---
+++ plus.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Pros of the Cloud</title>
+  <style>
+  body {
+	margin:0;
+	}
+
+	ul {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		overflow: hidden;
+		background-color: #282828;
+		position: fixed;
+		top: 0;
+		width: 100%;
+	}
+
+	li {
+		float: left;
+	}
+
+	li a {
+		display: block;
+		color: white;
+		text-align: center;
+		padding: 14px 16px;
+		text-decoration: none;
+	}
+
+	li a:hover {
+		background-color: #333333;
+	}
+
+	a:hover {
+		color: white;
+	}
+
+	body {
+	background-color: white;
+	color: black;
+	font-family: Lucida Console;
+	}
+  </style>
+</head>
+<body>
+  <ul>
+    <li><a href="home.html">Home</a></li>
+    <li><a href="plus.html">Why Cloud is good</a></li>
+    <li><a href="minus.html">Why Cloud isn't good</a></li>
+    <li><a href="ourcloudlinkedhere">Our Cloud Server</a></li>
+  </ul>
+</body>
+</html>
